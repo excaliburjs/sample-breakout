1a849d838f3d4271c4b9f2ad8ddda6f6bf38bbff
eonarheim <erik.onarheim@gmail.com>
2021-12-28 20:14:01 +0000

    Deploy Breakout

--- test/test.js
+++
@@ -1,7 +0,0 @@
-const { expectLoaded, expectPage, test } = require("@excaliburjs/testing");
-
-test("A breakout sample", async (page) => {
-  await expectPage("Shows bricks", "./test/images/actual-loaded.png").toBe(
-    "./test/images/expected-loaded.png"
-  );
-});
